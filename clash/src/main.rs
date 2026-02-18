0789f8ef9f180c9455518c8852c3ef1cac2f93df
Eliot Hedeman <eliot.d.hedeman@gmail.com>
2026-02-18 13:34:32 -0500

    fixed clester test

--- clash/src/main.rs
+++ clash/src/main.rs
@@ -884,10 +884,7 @@ fn run_init(no_bypass: Option<bool>, project: Option<bool>) -> Result<()> {
             .interact()
             .unwrap_or_default()
     {
-        anyhow::bail!(
-            "Cowardly refusing to configure clash due to existin policy at {}\n",
-            sexpr_path.display()
-        );
+        return Ok(());
     }
 
     let yaml_path = ClashSettings::legacy_policy_file()?;
